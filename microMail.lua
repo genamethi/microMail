2f4a50f45636d438647e52a8618f3bf1ff1971a7
Eric Page <genamthi@gmail.com>
2012-04-26 22:00:52 -0400

    Testing something...

--- microMail.lua
+++ microMail.lua
@@ -179,6 +179,37 @@ function Send( sSender, sRec, sMsg, sSubj )
 	end
 end
 
+-- Serialize by nErBoS
+
+function SaveToFile( file, Table, tablename, mode )
+	local assert, type = assert, type;
+	local function Serialize(tTable, sTableName, sTab)
+		assert(tTable, "tTable equals nil");
+		assert(sTableName, "sTableName equals nil");
+		assert(type(tTable) == "table", "tTable must be a Table!");
+		assert(type(sTableName) == "string", "sTableName must be a string!");
+		sTab = sTab or "";
+		sTmp = ""
+		sTmp = sTmp..sTab..sTableName.." = {\n"
+		local s_format, tostring = string.format, tostring;
+		for key, value in pairs( tTable ) do
+			local sKey = ( type( key ) == "string" ) and s_format( "[%q]", key ) or s_format( "[%d]", key );
+			if(type(value) == "table") then
+				sTmp = sTmp..Serialize(value, sKey, sTab.."\t");
+			else
+				local sValue = ( type( value ) == "string") and s_format( "%q", value ) or tostring( value );
+				sTmp = sTmp..sTab.."\t"..sKey.." = "..sValue
+			end
+			sTmp = sTmp..",\n"
+		end
+		sTmp = sTmp..sTab.."}\n"
+		return sTmp
+	end
+	local handle = io.open( file, mode );
+	handle:write( Serialize( Table, tablename ) );
+	handle:close( );
+end
+
 
 tCommandArrivals = {	
 	wmail = {
@@ -215,7 +246,7 @@ tCommandArrivals = {
 	},
 }
 function tCommandArrivals.mhelp:Action( tUser )
-	local sRet = "\n\n**-*-** " .. ScriptMan.GetScript().sName .."  help (use one of these prefixes: " .. SetMan.GetString( 29 ) .. " Works in main or in PM to " .. tMail[1] .. " **-*-**\n\n";
+	local sRet = "\n\n**-*-** " .. ScriptMan.GetScript().sName .."  help (use one of these prefixes: " .. SetMan.GetString( 29 ) .. ") Works in main or in PM to " .. tMail[1] .. " **-*-**\n\n";
 	for name, obj in pairs( tCommandArrivals ) do
 		if obj.Permissions[ tUser.iProfile ] then
 			sRet = sRet .. name .. obj.sHelp;
